d25ed8ee3472a5fd7c26a7734d2d5822ed90805e
Thomas <thomasnelson2123@gmail.com>
2023-04-30 12:58:24 -0700

    Added XML parser class

---
+++ ParseXML.java
@@ -0,0 +1,15 @@
+import javax.swing.text.Document;
+
+public class ParseXML {
+    public Document getDocFromFile(String file) {
+        return null;
+    }
+
+    public void readBoardData(Document d) {
+
+    }
+
+    public void readCardData(Document d) {
+        
+    }
+}
