bf1d4da6c3609e4144a39a383a66b7f56e18abea
Carter Staley <123carter2@gmail.com>
2023-05-01 19:57:51 -0700

    made it not crash

--- Classes/Deadwood.java
+++ Classes/Deadwood.java
@@ -11,8 +11,17 @@ public class Deadwood{
         GameManager manager;
         ParseXML parser = new ParseXML();
 
+        /*
+        File f = new File("../XML_Files/board.xml");
+
+
+        if(!f.exists()){
+            f = new File("XML_Files/board.xml");
+        }
+
         Room[] rooms = parser.readBoardData(parser.getDocFromFile("../XML_Files/board.xml"));
         Scene[] scenes = parser.readCardData(parser.getDocFromFile("../XML_Files/cards.xml"));
+        */
     }
 
     // initializes game manager, board, players, etc
